837ea89450278ecd7564b3856d4a7382f090f59e
Thomas Hudspith-Tatham <iammotivated@gmail.com>
2026-03-14 19:19:16 +0000

    component: migrate to css-modules

--- components/separator/src/Separator.tsx
+++ components/separator/src/Separator.tsx
@@ -1,4 +1,6 @@
 import type { HTMLAttributes } from 'react'
+import clsx from 'clsx'
+import styles from './separator.module.css'
 
 export type SeparatorProps = HTMLAttributes<HTMLHRElement> & {
   isDecorative?: boolean
@@ -7,15 +9,12 @@ export type SeparatorProps = HTMLAttributes<HTMLHRElement> & {
 export function Separator(props: SeparatorProps) {
   const { className, isDecorative, ...rest } = props
   const ariaHidden = isDecorative ? true : rest['aria-hidden']
-  const rootClassName = className
-    ? `arachne-separator ${className}`
-    : 'arachne-separator'
 
   return (
     <hr
       {...rest}
       aria-hidden={ariaHidden}
-      className={rootClassName}
+      className={clsx(styles.base, className)}
     />
   )
 }
